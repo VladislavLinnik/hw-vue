029334e392bea6ec65ba29c05768e1ea17e945f9
Vladislav Linnik <vladik.linnik.1999@mail.ru>
2019-04-19 06:41:07 +0300

    Markup

--- hw-vue-1/main.js
+++ hw-vue-1/main.js
@@ -1,6 +1,14 @@
 var app = new Vue({
     el: '#app',
     data: {
-    
-    }
+        // email: ,
+        // phone: ,
+        form_sended: false
+    },
+    // methods: {
+        
+    // },
+    // computed: {
+
+    // }
 });
