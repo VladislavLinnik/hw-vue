9e7ce1f7808ba789dcf2cf000c2cf6cbf93b23d1
Vlad Linnik <vladislav.linnik.1999@gmail.com>
2019-04-26 12:54:37 +0300

    some changes

--- hw-vue-1/main.js
+++ hw-vue-1/main.js
@@ -15,12 +15,23 @@ var app = new Vue({
 			this.users.push(Math.random());
 		},
         send(){
-            this.form_sended = !this.form_sended;
-            this.hideForm = !this.hideForm;
-            var inputs = document.querySelectorAll('.input');
-			for (var i = 0; i < inputs.length; i++){
-				this.tests.push(inputs[i].value)
-			}
+            var email = document.querySelector('#email').value,
+                first_name = document.querySelector('#first_name').value,
+                last_name = document.querySelector('#last_name').value,
+                phone = document.querySelector('#phone').value;
+            
+            if(email != '' && first_name != '' && last_name != '' && phone != '') {
+                this.form_sended = !this.form_sended;
+                this.hideForm = !this.hideForm;
+                
+                var inputs = document.querySelectorAll('.input');
+                for (var i = 0; i < inputs.length; i++){
+                    this.tests.push(inputs[i].value)
+                }
+			} else {
+                alert('Please, fill in all fields!');
+            }
+            
         },
         deleteGuest(index){
             console.log(index);
